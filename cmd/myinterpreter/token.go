1f9b1986287eb1bb421661d4f91a539da4f75096
Alex Kyriacou <a.kyriacou14@gmail.com>
2024-12-27 19:27:32 +1100

    fix(scanner): ensure string representation for NUMBER token type always has one decimal place

--- cmd/myinterpreter/token.go
+++ cmd/myinterpreter/token.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Token struct {
 	tokenType TokenType
@@ -13,6 +15,15 @@ func (t Token) String() string {
 	var literalStr string
 	if t.literal == nil {
 		literalStr = "null"
+	} else if t.tokenType == NUMBER {
+		// This is here as go prints a 1.0 float as 1 and we want a minimum 
+		// of one decimal place to pass the tests
+		// if this is no longer a requirement, we can remove this check
+		if t.literal.(float64) == float64(int(t.literal.(float64))) {
+			literalStr = fmt.Sprintf("%.1f", t.literal)
+		} else {
+			literalStr = fmt.Sprintf("%g", t.literal)
+		}
 	} else {
 		literalStr = fmt.Sprintf("%v", t.literal)
 	}
